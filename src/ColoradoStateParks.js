106f53694a4639c2f17350ef47b063ba7cd778e8
Gladys Ndichu <itsgladysbrian@gmail.com>
2022-07-16 10:03:54 +0300

    Mesaverde added

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  MesaVerde();
 
   return <h1>Colorado State Parks!</h1>;
+  
+ 
+  
 }
+
+
+export default ColoradoStateParks;
